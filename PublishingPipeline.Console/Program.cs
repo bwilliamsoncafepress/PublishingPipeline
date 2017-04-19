34bd7abcc76cdf362099d95a595f082ab7900682
Ben Williamson <bwilliamson@cafepress.com>
2017-04-18 22:07:52 -0400

    Feature/rt 2
    * Commit 1
    
    * Commit 2
    
    * Commit 3

--- PublishingPipeline.Console/Program.cs
+++ PublishingPipeline.Console/Program.cs
@@ -8,8 +8,11 @@ namespace PublishingPipeline.Console
         static void Main(string[] args)
         {
             console.WriteLine("rt-1 - Commit 1");
+            console.WriteLine("rt-2 - Commit 1");
             console.WriteLine("rt-1 - Commit 2");
+            console.WriteLine("rt-2 - Commit 2");
             console.WriteLine("rt-1 - Commit 4");
+            console.WriteLine("rt-2 - Commit 3");
             console.WriteLine("rt-1 - Commit 5");
         }
     }
