948a1b4d93b7e57eba25a45962ce45160c5a2e8e
Ben Williamson <bwilliamson@cafepress.com>
2017-04-24 14:30:37 -0400

    Feature 5 commit 1

--- PublishingPipeline.Console/Program.cs
+++ PublishingPipeline.Console/Program.cs
@@ -16,6 +16,7 @@ namespace PublishingPipeline.Console
             console.WriteLine("rt-1 - Commit 5");
             console.WriteLine("rt-3 - Commit 1");
             console.WriteLine("rt-4 - Commit 1");
+            console.WriteLine("rt-5 - Commit 1");
         }
     }
 }
